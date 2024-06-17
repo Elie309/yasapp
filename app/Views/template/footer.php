88be03c31ba66ae3ea69772436626bf4bb74797c
Elie Saade <elie.saade432@gmail.com>
2024-06-17 18:20:06 +0300

    Remove code from navigation update

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -1,17 +1,8 @@
-
 </div>
 <script>
         document.getElementById('sidebar-toggle').addEventListener('click', function() {
             const sidebar = document.getElementById('sidebar');
             sidebar.classList.toggle('hidden');
-
-            if(!sidebar.classList.contains('hidden')){
-                sidebar.classList.add('absolute w-screen')
-            }else{
-                sidebar.classList.remove('absolute w-screen')
-            }
-
-           
         });
     </script>
 
