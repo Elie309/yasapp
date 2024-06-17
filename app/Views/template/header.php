8120525cf13e0c06cde0275f4b83fde304d67c03
Elie Saade <elie.saade432@gmail.com>
2024-06-17 18:56:10 +0300

    Update header.php

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -8,7 +8,7 @@
     <link href="css/output.css" rel="stylesheet">
 </head>
 
-<body class="bg-gray-100 flex flex-col min-h-screen">
+<body class=" bg-gray-100 flex flex-col min-h-screen overflow-hidden">
     <!-- Top Navigation Bar -->
     <nav class="md:hidden bg-gray-800 text-white p-4 flex justify-between">
         <img src="logo.png" alt="Logo" class="w-24">
@@ -17,15 +17,21 @@
         </button>
     </nav>
 
-    <div class="inline-block md:flex md:flex-1">
+    <div class="w-full h-full inline-block md:flex md:flex-1">
 
         <!-- Sidebar -->
-        <div id="sidebar" class="hidden bg-gray-700 md:bg-gray-800 text-white p-4 transition-width delay-150 md:w-1/4 md:max-w-60 md:block">
+        <div id="sidebar" class="hidden
+                            absolute z-50 w-full min-h-full bg-gray-700
+                            md:relative md:block md:w-1/4 md:max-w-60 md:bg-gray-800 
+                            text-white p-4 
+                           ">
+
+
             <div class="hidden md:block mb-8">
                 <img src="logo.png" alt="Logo" class="w-full max-w-60 max-h-32 mx-auto md:mx-0">
             </div>
             <nav>
-                <ul class="font-bold">
+                <ul class="font-bold text-center md:text-start">
                     <li class="mb-4"><a href="" class="block p-4 rounded hover:bg-red-800">Listings</a></li>
                     <li class="mb-4"><a href="" class="block p-4 rounded hover:bg-red-800">Requests</a></li>
                     <li class="mb-4"><a href="" class="block p-4 rounded hover:bg-red-800">Profile</a></li>
