f949dbdbde6c8389e35aeed0a65a911e49d39dbb
Elie Saade <elie.saade432@gmail.com>
2024-06-17 18:18:38 +0300

    Navigation update

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -1,2 +1,20 @@
+
+</div>
+<script>
+        document.getElementById('sidebar-toggle').addEventListener('click', function() {
+            const sidebar = document.getElementById('sidebar');
+            sidebar.classList.toggle('hidden');
+
+            if(!sidebar.classList.contains('hidden')){
+                sidebar.classList.add('absolute w-screen')
+            }else{
+                sidebar.classList.remove('absolute w-screen')
+            }
+
+           
+        });
+    </script>
+
 </body>
+
 </html>
