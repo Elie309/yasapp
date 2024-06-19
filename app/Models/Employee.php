af14ef6ccb21dad15f388ee39acd2c77f0b32d33
Elie309 <elie.saade432@gmail.com>
2024-06-19 16:57:51 +0300

    Employee class Modification

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -9,7 +9,8 @@ class Employee extends Model
     protected $table            = 'employees';
     protected $primaryKey       = 'employee_id';
     protected $useAutoIncrement = true;
-    protected $returnType       = 'App\Entities\Employee';
+    protected $returnType       = \App\Entities\Employee::class;
+
     protected $allowedFields    = ["employee_name", "employee_password", "employee_role"];
 
     protected bool $allowEmptyInserts = false;
