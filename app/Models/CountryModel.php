1477962c51a8203b463a71552ffddb23fd1d8d99
Elie309 <elie.saade432@gmail.com>
2024-06-19 17:10:05 +0300

    CountryModel

---
+++ app/Models/CountryModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Country extends Model
+{
+    protected $table            = 'countries';
+    protected $primaryKey       = 'country_id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    protected $useTimestamps = false;
+
+
+
+}
