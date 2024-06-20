40be111f5c1f57749b0e737a907861ab86447ab2
Elie309 <elie.saade432@gmail.com>
2024-06-20 23:16:36 +0300

    Relocation

--- app/Views/settings/settings.php
+++ app/Views/settings/settings.php
@@ -1,8 +1,6 @@
-<div class="bg-gray-100 min-h-screen flex items-start justify-center">
+<div class="w-full bg-gray-100 min-h-screen flex items-start justify-center">
     <div class="container mx-auto p-4 w-full">
         <h1 class="text-4xl font-bold text-center mb-8">Settings</h1>
-        <div class="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-            <?= view_cell('\App\Cells\SettingsCell::render') ?>
-        </div>
+        <?= view_cell('\App\Cells\Settings\SettingsCell::render') ?>
     </div>
 </div>
