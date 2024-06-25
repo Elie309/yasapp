93d77dd4843c840e3b55486c5a71bf2aa618d4e7
Elie309 <elie.saade432@gmail.com>
2024-06-25 23:25:56 +0300

    Array to entity

--- app/Cells/Settings/Location/LocationTable/location_table.php
+++ app/Cells/Settings/Location/LocationTable/location_table.php
@@ -11,7 +11,7 @@
         <?php foreach ($countries as $country) : ?>
             <div class="mb-6">
 
-                <h2 class="text-2xl font-bold mb-2"><?= esc($country['country_name']) ?></h2>
+                <h2 class="text-2xl font-bold mb-2"><?= esc($country->country_name) ?></h2>
 
                 <table class="w-full table-auto">
                     <thead>
@@ -39,17 +39,17 @@
 
 
                     <tbody>
-                        <?php foreach ($country['regions'] as $region) : ?>
+                        <?php foreach ($country->regions as $region) : ?>
                             <!-- To know how much we need to span the first col
                                  we need to know how many cities we have that will decide the span of the row -->
                             <?php
                             $totalCities = 0;
-                            foreach ($region['subregions'] as $subregion) {
-                                $totalCities += count($subregion['cities']);
+                            foreach ($region->subregions as $subregion) {
+                                $totalCities += count($subregion->cities);
 
                                 // If we have no cities inside a subregion we need to now show anything
                                 //so we need to add one for the count
-                                if (count($subregion['cities']) === 0) {
+                                if (count($subregion->cities) === 0) {
                                     $totalCities += 1;
                                 }
                             }
@@ -64,28 +64,28 @@
                             <tr>
                                 <!-- REGION -->
                                 <td class="border px-4 py-2 align-top" rowspan="<?= $totalCities ?>">
-                                    <?= esc($region['region_name']) ?>
+                                    <?= esc($region->region_name) ?>
                                 </td>
 
                                 <!-- SUBREGION -->
-                                <?php foreach ($region['subregions'] as $subregionIndex => $subregion) : ?>
+                                <?php foreach ($region->subregions as $subregionIndex => $subregion) : ?>
                                     <?php if ($subregionIndex > 0) : ?>
                             <tr>
                             <?php endif; ?>
 
 
-                            <td class="border px-4 py-2 align-top" rowspan="<?= count($subregion['cities']) === 0 ? 1 : count($subregion['cities']) ?>">
-                                <?= esc($subregion['subregion_name']) ?>
+                            <td class="border px-4 py-2 align-top" rowspan="<?= count($subregion->cities) === 0 ? 1 : count($subregion->cities) ?>">
+                                <?= esc($subregion->subregion_name) ?>
                             </td>
 
                             <!-- CITIES  -->
-                            <?php foreach ($subregion['cities'] as $cityIndex => $city) : ?>
+                            <?php foreach ($subregion->cities as $cityIndex => $city) : ?>
                                 <?php if ($cityIndex > 0) : ?>
-                            <tr>
-                            <?php endif; ?>
-                            <td class="border px-4 py-2">
-                                <?= esc($city['city_name']) ?>
-                            </td>
+                                    <tr>
+                                <?php endif; ?>
+                                <td class="border px-4 py-2">
+                                    <?= esc($city->city_name) ?>
+                                </td>
                             </tr>
                         <?php endforeach; ?>
 
