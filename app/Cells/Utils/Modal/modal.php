f18f38f78ef53c5b91594be4eb87fad4179ef3a6
Elie309 <elie.saade432@gmail.com>
2024-07-21 13:37:06 +0300

    chore: Update modal.php to use min-height instead of fixed height for better responsiveness

--- app/Cells/Utils/Modal/modal.php
+++ app/Cells/Utils/Modal/modal.php
@@ -1,6 +1,6 @@
 <div id="<?= $modalId ?>" class="hidden fixed w-full h-full inset-0 bg-gray-900 bg-opacity-50 items-center justify-center">
 
-    <div class="bg-white sm:rounded-lg h-full overflow-auto sm:h-5/6 w-full sm:w-3/4 lg:w-1/2">
+    <div class="bg-white sm:rounded-lg overflow-auto sm:min-h-fit w-full sm:w-3/4 lg:w-1/2">
         <div class="flex justify-between w-full text-center p-4 shadow-xl">
             <h3 class="text-xl font-semibold"><?= $modalTitle ?></h3>
             <button class="<?= $closeButtonClass ?>" onclick="closeModal('<?= $modalId ?>')"><?= $closeButtonText ?></button>
