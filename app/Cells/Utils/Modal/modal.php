9b710e236d675c2f1c8e2cc64416b87ca630c980
Elie309 <elie.saade432@gmail.com>
2024-07-27 00:08:39 +0300

    refactor: Update modal layout and styles for better user experience

--- app/Cells/Utils/Modal/modal.php
+++ app/Cells/Utils/Modal/modal.php
@@ -1,6 +1,7 @@
-<div id="<?= $modalId ?>" class="hidden fixed w-full h-full inset-0 bg-gray-900 bg-opacity-50 items-center justify-center">
+<div id="<?= $modalId ?>" class="hidden fixed w-full min-h-full inset-0 
+            bg-gray-900 bg-opacity-50 items-center justify-center py-10">
 
-    <div class="bg-white sm:rounded-lg overflow-auto sm:min-h-fit w-full sm:w-3/4 lg:w-1/2">
+    <div class="bg-white sm:rounded-lg overflow-auto sm:min-h-fit w-full sm:w-5/6 lg:w-3/4 xl:w-2/6 max-h-screen my-4">
         <div class="flex justify-between w-full text-center p-4 shadow-xl">
             <h3 class="text-xl font-semibold"><?= $modalTitle ?></h3>
             <button class="<?= $closeButtonClass ?>" onclick="closeModal('<?= $modalId ?>')"><?= $closeButtonText ?></button>
