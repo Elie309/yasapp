bce13a9ae6e4577f250740aa40a160310703d1bf
Elie309 <elie.saade432@gmail.com>
2024-09-03 23:31:12 +0300

    refactor: Add tables for property listings
    - Added tables for properties, land details, apartment gender, apartment details, apartment partition, and apartment specifications in the DATABASE.sql file.
    - Created the necessary columns and foreign key constraints for each table.
    - This commit refactors the database structure to accommodate property listings and their specific details.

--- app/Database/DATABASE.sql
+++ app/Database/DATABASE.sql
@@ -130,3 +130,134 @@ CREATE TABLE IF NOT EXISTS Requests (
 
     FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
 );
+
+
+-- LISTINGS
+
+CREATE TABLE properties (
+    property_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+    
+    client_id INT UNSIGNED NOT NULL,
+    employee_id INT UNSIGNED NOT NULL,
+    payment_plan_id INT UNSIGNED NOT NULL,
+
+    city_id INT UNSIGNED NOT NULL,
+    property_location VARCHAR(255) NOT NULL,
+
+    property_referral_name VARCHAR(255),
+    property_referral_phone VARCHAR(20),
+
+    property_size DECIMAL(10, 2),
+    property_type ENUM('land', 'apartment'),
+    property_rent_or_sale ENUM('rent', 'sale', 'rent_sale'),
+    property_catch_phrase TEXT,
+
+    property_price DECIMAL(15, 2),
+    
+
+    FOREIGN KEY (client_id) REFERENCES Clients(client_id),
+    FOREIGN KEY (city_id) REFERENCES Cities(city_id),
+    FOREIGN KEY (payment_plan_id) REFERENCES PaymentPlans(payment_plan_id),
+    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
+);
+
+
+CREATE TABLE land_details (
+    land_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+    property_id INT UNSIGNED NOT NULL UNIQUE,
+
+    land_type ENUM('residential', 'industrial', 'commercial') DEFAULT 'residential',
+    land_zone_first DECIMAL(5, 2),
+    land_zone_second DECIMAL(5, 2),
+    land_extra_features TEXT,
+    FOREIGN KEY (property_id) REFERENCES properties(property_id)
+);
+
+CREATE TABLE apartment_gender (
+    ag_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    apartment_gender VARCHAR(255) NOT NULL
+);
+
+
+
+
+CREATE TABLE apartment_details (
+
+    apartment_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+    property_id INT UNSIGNED NOT NULL UNIQUE,
+
+    ad_terrace_or_garden BOOLEAN,
+    ad_terrace_area INT,
+    ad_roof BOOLEAN,
+    ad_roof_area INT,
+
+    ad_gender_id INT UNSIGNED NOT NULL,
+    
+    ad_furnished BOOLEAN,
+    ad_furnished_on_provisions BOOLEAN,
+    ad_elevator BOOLEAN,
+
+    ad_status_age VARCHAR(255),
+    ad_floor_level INT,
+    ad_apartments_per_floor INT,
+    ad_view VARCHAR(255),
+    ad_type ENUM('Luxury', 'High-end', 'Bad'),
+    ad_architecture_and_interior TEXT,
+    ad_extra_features TEXT,
+
+    FOREIGN KEY (property_id) REFERENCES properties(property_id),
+    FOREIGN KEY (ad_gender_id) REFERENCES  apartment_gender(ag_id)
+);
+
+
+CREATE TABLE apartment_partition (
+    partition_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+    apartment_id INT UNSIGNED NOT NULL UNIQUE,
+
+    partition_salon VARCHAR(255),
+    partition_dining VARCHAR(255),
+    partition_kitchen VARCHAR(255),
+    partition_master_bedroom VARCHAR(255),
+    partition_bedroom VARCHAR(255),
+    partition_bathroom VARCHAR(255),
+    partition_maid_room VARCHAR(255),
+    partition_reception_balcony VARCHAR(255),
+    partition_sitting_corner VARCHAR(255),
+    partition_balconies VARCHAR(255),
+    partition_parking VARCHAR(255),
+    partition_storage_room VARCHAR(255),
+
+    FOREIGN KEY (apartment_id) REFERENCES apartment_details(apartment_id)
+);
+
+CREATE TABLE apartment_specifications (
+    spec_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    apartment_id INT UNSIGNED NOT NULL UNIQUE,
+
+    spec_heating_system BOOLEAN,
+    spec_heating_system_on_provisions BOOLEAN,
+    spec_ac_system BOOLEAN,
+    spec_ac_system_on_provisions BOOLEAN,
+    spec_double_wall BOOLEAN,
+    spec_double_glazing BOOLEAN,
+    spec_shutters_electrical BOOLEAN,
+    spec_tiles ENUM('European', 'Marble', 'Granite', 'Other'),
+    spec_oak_doors BOOLEAN,
+    spec_chimney BOOLEAN,
+    spec_indirect_light BOOLEAN,
+    spec_wood_panel_decoration BOOLEAN,
+    spec_stone_panel_decoration BOOLEAN,
+    spec_security_door BOOLEAN,
+    spec_alarm_system BOOLEAN,
+    spec_solar_heater BOOLEAN,
+    spec_intercom BOOLEAN,
+    spec_garage BOOLEAN,
+    
+
+    FOREIGN KEY (apartment_id) REFERENCES apartment_details(apartment_id)
+);
