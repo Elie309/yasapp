14e86dc3cfd6f0d52bf885605d810c724f8184e3
Elie309 <elie.saade432@gmail.com>
2024-09-05 22:42:53 +0300

    feat: Improve error handling in error_handler.php
    - Add support for displaying multiple errors in a list format
    - Escape error messages to prevent potential security vulnerabilities
    - Enhance user experience by providing clear error messages
    
    This commit improves the error handling functionality in the `error_handler.php` file. It introduces the ability to display multiple errors in a list format, ensuring that all errors are visible to the user. Additionally, it escapes the error messages to prevent potential security vulnerabilities. These enhancements aim to provide a better user experience by presenting clear and informative error messages.

--- app/Cells/Utils/ErrorHandler/error_handler.php
+++ app/Cells/Utils/ErrorHandler/error_handler.php
@@ -1,10 +1,16 @@
 <?php if (session()->has('errors')) : ?>
     <div class="error-div" role="alert">
+        <?php if (is_array(session('errors'))) : ?>
+            <ul>
+                <?php foreach (session('errors') as $error) : ?>
+                    <li><?= esc($error) ?></li>
+                <?php endforeach ?>
+            </ul>
+        <?php else : ?>
         <ul>
-            <?php foreach (session('errors') as $error) : ?>
-                <li><?= esc($error) ?></li>
-            <?php endforeach ?>
+            <li><?= esc(session('errors')) ?></li>
         </ul>
+        <?php endif ?>
     </div>
 <?php endif; ?>
 
