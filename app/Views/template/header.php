623673f6f3793e9d67984646068fae422fe8c6bc
Elie309 <elie.saade432@gmail.com>
2024-09-05 22:43:44 +0300

    refactor: Update title tag in header.php to use variable

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title><?= $title ?? 'Dashboard' ?></title>
     <link href="<?= base_url('css/output.css') ?>" rel="stylesheet">
 </head>
 
