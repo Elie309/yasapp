8196f5bed8ac35b7f63197f96dbe179ec8046228
Elie309 <elie.saade432@gmail.com>
2024-09-08 14:26:06 +0300

    Slight modification in styles

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -21,23 +21,24 @@
 
         <!-- Sidebar -->
         <div id="sidebar" class="hidden
-                            w-full min-h-full bg-gray-700
-                            md:relative md:block md:w-1/4 md:max-w-60 md:bg-gray-800 
-                            text-white p-4 
+                            w-full min-h-full bg-[#414652]
+                            md:relative md:block md:w-1/4 md:max-w-60 md:bg-gray-900 
+                            text-white p-4 shadow-lg
                            ">
 
 
             <div class="hidden md:block mb-8">
-                <img src="<?= base_url('logo.png') ?>" alt="Logo" class="w-full max-w-60 max-h-32 mx-auto md:mx-0">
+                <a href="/" class="outline-none focus:outline-red-800">
+                    <img src="<?= base_url('logo.png') ?>" alt="Logo" class="w-full max-w-60 max-h-32 mx-auto md:mx-0">
+                </a>
             </div>
             <nav>
                 <ul class="font-bold text-center md:text-start">
-                    <li class="mb-4"><a href="/clients" class="block p-4 rounded hover:bg-red-800">Clients</a></li>
-                    <li class="mb-4"><a href="/listings" class="block p-4 rounded hover:bg-red-800">Listings</a></li>
-                    <li class="mb-4"><a href="/requests" class="block p-4 rounded hover:bg-red-800">Requests</a></li>
-                    <li class="mb-4"><a href="/settings" class="block p-4 rounded hover:bg-red-800">Settings</a></li>
-
-                    <li class="mb-4"><a href="/logout" class="block p-4 rounded hover:bg-red-800">Logout</a></li>
+                    <li class="mb-4"><a href="/clients" class="block p-4 rounded hover:bg-red-800 outline-none focus:outline-red-800">Clients</a></li>
+                    <li class="mb-4"><a href="/listings" class="block p-4 rounded hover:bg-red-800 outline-none focus:outline-red-800">Listings</a></li>
+                    <li class="mb-4"><a href="/requests" class="block p-4 rounded hover:bg-red-800 outline-none focus:outline-red-800">Requests</a></li>
+                    <li class="mb-4"><a href="/settings" class="block p-4 rounded hover:bg-red-800 outline-none focus:outline-red-800">Settings</a></li>
+                    <li class="mb-4"><a href="/logout" class="block p-4 rounded hover:bg-red-800 outline-none focus:outline-red-800">Logout</a></li>
                 </ul>
             </nav>
         </div>
