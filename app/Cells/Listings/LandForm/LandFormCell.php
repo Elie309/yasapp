4c40b5127112f45809f418d3715c0a67b0cedb18
Elie309 <elie.saade432@gmail.com>
2024-10-15 23:58:14 +0300

    Add LandFormCell and land_form.php for managing land details in listings

---
+++ app/Cells/Listings/LandForm/LandFormCell.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Cells\Listings\LandForm;
+
+use CodeIgniter\View\Cells\Cell;
+
+class LandFormCell extends Cell
+{
+    public $landTypes;
+}
---
+++ app/Cells/Listings/LandForm/land_form.php
@@ -0,0 +1,27 @@
+<div class="my-2">
+    <h2 class="secondary-title">Land Details</h2>
+
+    <div class="my-4">
+        <label class="main-label" for="land_type">Land Type:</label>
+        <select id="land_type" name="land_type" class="main-input">
+            <?php foreach ($landTypes as $landType) : ?>
+                <option value="<?= $landType ?>"><?= ucfirst($landType) ?></option>
+            <?php endforeach; ?>
+        </select><br>
+
+    </div>
+    <div class="my-4">
+        <label class="main-label" for="land_zone_first">Zone First (%):</label>
+        <input type="number" step="0.1" id="land_zone_first" name="land_zone_first" class="main-input">
+    </div>
+
+    <div class="my-4">
+        <label class="main-label" for="land_zone_second">Zone Second (%):</label>
+        <input type="number" step="0.1" id="land_zone_second" name="land_zone_second" class="main-input">
+    </div>
+    <div class="my-4">
+
+        <label class="main-label" for="land_extra_features">Extra Features:</label>
+        <textarea id="land_extra_features" name="land_extra_features" class="main-input"></textarea>
+    </div>
+</div>
