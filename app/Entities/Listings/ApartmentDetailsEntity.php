2fbfbe5516cc4927075e449759119c7ecc72fd6f
Elie309 <elie.saade432@gmail.com>
2024-10-29 23:33:48 +0200

    Changing the 'on' value of the form input to a boolean value that will be checked in the model

--- app/Entities/Listings/ApartmentDetailsEntity.php
+++ app/Entities/Listings/ApartmentDetailsEntity.php
@@ -10,16 +10,43 @@ class ApartmentDetailsEntity extends Entity
     protected $casts = [
         'apartment_id' => 'int',
         'property_id' => 'int',
-        'ad_terrace' => 'bool',
-        'ad_roof' => 'bool',
         'ad_gender_id' => 'int',
-        'ad_furnished' => 'bool',
-        'ad_furnished_on_provisions' => 'bool',
-        'ad_elevator' => 'bool',
-        'ad_status_age' => 'int',
+        'ad_status_age' => 'string',
         'ad_apartments_per_floor' => 'int',
         'ad_terrace_area' => 'int',
         'ad_roof_area' => 'int',
         'ad_floor_level' => 'int',
     ];
+
+
+    public function setAdTerrace(string $value)
+    {
+        $this->attributes['ad_terrace'] = $value == 'on' ? true : false;
+    }
+
+    public function setAdRoof(string $value)
+    {
+        $this->attributes['ad_roof'] = $value == 'on' ? true : false;
+    }
+
+    public function setAdFurnished(string $value)
+    {
+        $this->attributes['ad_furnished'] = $value == 'on' ? true : false;
+    }
+
+    public function setAdFurnishedOnProvisions(string $value)
+    {
+        $this->attributes['ad_furnished_on_provisions'] = $value == 'on' ? true : false;
+    }
+
+    public function setAdElevator(string $value)
+    {
+        $this->attributes['ad_elevator'] = $value == 'on' ? true : false;
+    }
+
+
+
+
+
+    
 }
--- app/Entities/Listings/ApartmentSpecificationsEntity.php
+++ app/Entities/Listings/ApartmentSpecificationsEntity.php
@@ -7,4 +7,97 @@ use CodeIgniter\Entity\Entity;
 class ApartmentSpecificationsEntity extends Entity
 {
     protected $datamap = [];
+    
+    protected $casts = [
+        'spec_id' => 'int',
+        'apartment_id' => 'int',
+        'spec_extra_features' => 'string'
+    ];
+
+    public function setSpecHeatingSystem(string $value)
+    {
+        $this->attributes['spec_heating_system'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecHeatingSystemOnProvisions(string $value)
+    {
+        $this->attributes['spec_heating_system_on_provisions'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecAcSystem(string $value)
+    {
+        $this->attributes['spec_ac_system'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecAcSystemOnProvisions(string $value)
+    {
+        $this->attributes['spec_ac_system_on_provisions'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecDoubleWall(string $value)
+    {
+        $this->attributes['spec_double_wall'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecDoubleGlazing(string $value)
+    {
+        $this->attributes['spec_double_glazing'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecShuttersElectrical(string $value)
+    {
+        $this->attributes['spec_shutters_electrical'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecOakDoors(string $value)
+    {
+        $this->attributes['spec_oak_doors'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecChimney(string $value)
+    {
+        $this->attributes['spec_chimney'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecIndirectLight(string $value)
+    {
+        $this->attributes['spec_indirect_light'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecWoodPanelDecoration(string $value)
+    {
+        $this->attributes['spec_wood_panel_decoration'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecStonePanelDecoration(string $value)
+    {
+        $this->attributes['spec_stone_panel_decoration'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecSecurityDoor(string $value)
+    {
+        $this->attributes['spec_security_door'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecAlarmSystem(string $value)
+    {
+        $this->attributes['spec_alarm_system'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecSolarHeater(string $value)
+    {
+        $this->attributes['spec_solar_heater'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecIntercom(string $value)
+    {
+        $this->attributes['spec_intercom'] = $value == 'on' ? true : false;
+    }
+
+    public function setSpecGarage(string $value)
+    {
+        $this->attributes['spec_garage'] = $value == 'on' ? true : false;
+    }
+
+
 }
