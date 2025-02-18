becaeccf3f0e8a8bcbceee7cbcff0dcaf0201fde
Elie309 <elie.saade432@gmail.com>
2025-02-18 17:05:56 +0200

    Refactor header styling in settings views for consistency

--- app/Views/settings/currencies.php
+++ app/Views/settings/currencies.php
@@ -1,5 +1,5 @@
 <div class="container-main">
-    <h1 class="text-4xl font-bold text-center mb-8">Settings</h1>
+    <h1 class="main-title-page">Settings</h1>
 
     <?= view_cell('\App\Cells\Settings\SettingsCell::render') ?>
 
